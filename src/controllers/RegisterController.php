9206f08dcf4522f9b35af63121c6d829e21604fd
Namibo06 <namibo654@gmail.com>
2024-10-19 00:10:09 -0300

    feat: added controller for create user

---
+++ src/controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace controllers;
+
+use models\UserModel;
+
+class RegisterController{
+    private $view;
+
+    public function __construct()
+    {
+        session_start();
+        $this->view = new \views\MainView('register');
+    }
+
+    /**Vai esperar cliques somente da página de registro, vai na model e retorna para página */
+    public function execute(){
+        if(isset($_POST['registerUserBtn'])){
+            $data = filter_input_array(INPUT_POST,FILTER_DEFAULT);
+
+            $createUser = UserModel::createUser($data);
+
+            if($createUser['status'] === 201){
+                echo '<meta http-equiv="refresh" content="0;url=login">';
+            }
+        }
+
+        $this->view->render(array('titulo' => 'Register'));
+    }
+}
