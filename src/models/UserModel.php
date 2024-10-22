30bc9c42672d757719cf1599252a0438f412a0e0
Namibo06 <namibo654@gmail.com>
2024-10-22 14:30:27 -0300

    feat: UserModel

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -2,29 +2,20 @@
 
 namespace models;
 
-use Connection;
 use Exception;
 use PDO;
 use PDOException;
-use services\TokenJwt;
+use Ramsey\Uuid\Uuid;
 
-class UserModel{
-    private static $pdo;
+class UserModel extends BaseModel{
+    public function __construct()
+    {
+        self::initConnection();
 
-    public static function messageError(string $typeError,string $messageError,int $statusCode):array{
-        return [
-            "message" => $typeError. ": ".$messageError,
-            "status" => $statusCode
-        ];
-    }
-
-    public static function initConnection(): void{
-        self::$pdo = Connection::Conn();
-
-        if(!self::$pdo){
-            UserModel::messageError("Conexão com PDO","PDO não inicializou",500);
+        if (!self::$pdo) {
+            self::messageError("Erro de conexão", "Não foi possível conectar ao banco de dados", 500);
         }
-    }
+    }    
 
     public static function verifyExistsByEmail(string $email):bool{
         $query = "
@@ -50,14 +41,63 @@ class UserModel{
         return $sql->rowCount() === 1;
     }
 
+    public static function addPhotoProfile($uuid,$file): array{
+        try{
+            if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['foto'])) {
+                $directory = "src/storage/profile/";
+    
+                if(!is_dir($directory)){
+                    mkdir($directory,0777,true);
+                }
+        
+                $filename = basename($file['name']);
+                $awayPhoto = $directory . $filename;
+        
+                if($file['error'] === UPLOAD_ERR_OK){
+                    if(move_uploaded_file($file['tmp_name'],$awayPhoto)){
+                        $query = "
+                            UPDATE tb_user 
+                            SET profile = ?
+                            WHERE uuid = ?
+                        ";
+                        $sql = self::$pdo->prepare($query);
+                        $sql->execute(array($filename, $uuid));
+        
+                        return [
+                            "message" => "Foto adicionada",
+                            "filename" => $filename,
+                            "status" => "200"
+                        ];
+                    }
+                }
+        
+                return [
+                    "message" => "Falha ao tentar adicionar foto",
+                    "status" => "500"
+                ];
+            }
+            
+        }catch(PDOException $e){
+            self::messageError("Conexão com PDO",$e->getMessage(),500);
+        }catch(Exception $e){
+            self::messageError("Geral",$e->getMessage(),400);
+        }finally{
+            return [
+                "message" => "Não foi possivel salvar foto",
+                "status" => "500"
+            ];
+        }
+    }
+
     public static function createUser(array $data): array{
-        $firstName = $data['first_name'];
-        $lastName = $data['last_name'];
+        $name = $data['name'];
         $email = $data['email'];
         $phone = $data['phone'];
         $password = password_hash($data['password'],PASSWORD_BCRYPT,['cost' => 10]);
 
         try{
+            $uuid = Uuid::uuid4();
+
             $existsEmail = UserModel::verifyExistsByEmail($email);
 
             if(!$existsEmail){
@@ -68,81 +108,37 @@ class UserModel{
             }
 
             $query = "INSERT INTO tb_user
-            (first_name,last_name,email,phone,password,created_at) 
+            (id,name,email,phone,password,created_at) 
             VALUES (?,?,?,?,?,NOW)
             ";
             $sql = self::$pdo->prepare($query);
 
-            if($sql->execute(array($firstName,$lastName,$email,$phone,$password))){
+            if($sql->execute(array($uuid,$name,$email,$phone,$password))){
                 return [
-                    "message" => "Sucesso: Usuário criado",
+                    "message" => "Usuário criado",
                     "status" => "201"
                 ];
             }
 
             return [
-                "message" => "Erro: Falha ao tentar cadastrar usuário",
+                "message" => "Falha ao tentar cadastrar usuário",
                 "status" => "400"
             ];
 
         }catch(PDOException $e){
-            UserModel::messageError("Conexão com PDO",$e->getMessage(),500);
+            self::messageError("Conexão com PDO",$e->getMessage(),500);
         }catch(Exception $e){
-            UserModel::messageError("Geral",$e->getMessage(),400);
-        }
-    }
-
-    public static function authenticate(array $data): array{
-        $email = $data['email'];
-        $password = $data['password'];
-
-        $query = "SELECT id,password FROM tb_user WHERE email = ?";
-        $sql = self::$pdo->prepare($query);
-        $sql->execute($email);
-        $result = self::$pdo->fetch(PDO::FETCH_OBJ);
-        $passwordWithHash = $result->password;
-        $id = $result->id;
-
-        $verifyPassword = password_verify($password,$passwordWithHash);
-
-        if(!$verifyPassword){
+            self::messageError("Geral",$e->getMessage(),400);
+        }finally{
             return [
-                "message" => "Senhas não batem",
-                "status" => 422
+                "message" => "Não foi possivel criar usuário",
+                "status" => "500"
             ];
-        }
-        
-        try{
-            $token = TokenJwt::createToken($id);
-
-            $query = "
-                UPDATE tb_user
-                SET token = ?
-                WHERE id = ?
-            ";
-            $sql = self::$pdo->prepare($query);
-            
-            if($sql->execute($token,$id)){
-                return [
-                    "message" => "Usuário logado com sucesso",
-                    "status" => 200
-                ];
-            }
-            
-            return [
-                "message" => "Não foi possivel realizar login",
-                "status" => 400
-            ];
-
-        }catch(PDOException $e){
-            UserModel::messageError("Conexão com PDO",$e->getMessage(),500);
-        }catch(Exception $e){
-            UserModel::messageError("Geral",$e->getMessage(),400);
         }
     }
 
     /**uma gestão de usuários para nós,fazer depois uns gráficos com chart.js */
-    public static function getAllUsers(): object{
+    public static function getAllUsers(): object | array{
         try{
             $query = "SELECT * FROM tb_users";
             $sql = self::$pdo->prepare($query);
@@ -158,13 +154,18 @@ class UserModel{
             return $sql->fetch(PDO::FETCH_OBJ);
 
         }catch(PDOException $e){
-            UserModel::messageError("Conexão com PDO",$e->getMessage(),500);
+            self::messageError("Conexão com PDO",$e->getMessage(),500);
         }catch(Exception $e){
-            UserModel::messageError("Geral",$e->getMessage(),400);
+            self::messageError("Geral",$e->getMessage(),400);
+        }finally{
+            return [
+                "message" => "Não foi possivel retornar usuários",
+                "status" => "500"
+            ];
         }
     }
 
-    public static function getUserByUuid(string $uuid): object{
+    public static function getUserByUuid(string $uuid): object | array{
         try{
             $query = "SELECT * FROM tb_users WHERE id = ?";
             $sql = self::$pdo->prepare($query);
@@ -180,15 +181,26 @@ class UserModel{
             return $sql->fetch(PDO::FETCH_OBJ);
 
         }catch(PDOException $e){
-            UserModel::messageError("Conexão com PDO",$e->getMessage(),500);
+            self::messageError("Conexão com PDO",$e->getMessage(),500);
         }catch(Exception $e){
-            UserModel::messageError("Geral",$e->getMessage(),400);
+            self::messageError("Geral",$e->getMessage(),400);
+        }finally{
+            return [
+                "message" => "Não foi possivel retornar usuário",
+                "status" => "500"
+            ];
         }
     }
 
     public static function updateUser(string $uuid, array $data): array{
-        $firstName = $data['first_name'];
-        $lastName = $data['last_name'];
+        if(!isset($_FILES['file'])){
+            $profile = null;
+        }
+        $profile =$_FILES['file'];
+        $addProfile = UserModel::addPhotoProfile($uuid,$profile);
+        $filenameProfile = $addProfile["filename"];
+
+        $name = $data['name'];
         $email = $data['email'];
         $phone = $data['phone'];
         $password = password_hash($data['password'],PASSWORD_BCRYPT,['cost' => 10]);
@@ -204,14 +216,15 @@ class UserModel{
 
             $query = "
                 UPDATE tb_user 
-                SET first_name = $firstName,
-                last_name = $lastName,
+                SET name = $name,
+                profile = $filenameProfile,
                 email = $email,
                 phone = $phone,
                 password = $password
                 updated_at = NOW
                 WHERE id = $uuid
             ";
+
             $sql = self::$pdo->prepare($query);
             
             if($sql->execute()){
@@ -222,14 +235,19 @@ class UserModel{
             }
             
             return [
-                "message" => "Não foi possivel atualizar usuário",
+                "message" => "Usuário não atualizado",
                 "status" => 400
             ];
 
         }catch(PDOException $e){
-            UserModel::messageError("Conexão com PDO",$e->getMessage(),500);
+            self::messageError("Conexão com PDO",$e->getMessage(),500);
         }catch(Exception $e){
-            UserModel::messageError("Geral",$e->getMessage(),400);
+            self::messageError("Geral",$e->getMessage(),400);
+        }finally{
+            return [
+                "message" => "Não foi possivel atualizar usuário",
+                "status" => "500"
+            ];
         }
     }
 
@@ -253,11 +271,14 @@ class UserModel{
             ];
 
         }catch(PDOException $e){
-            UserModel::messageError("Conexão com PDO",$e->getMessage(),500);
+            self::messageError("Conexão com PDO",$e->getMessage(),500);
         }catch(Exception $e){
-            UserModel::messageError("Geral",$e->getMessage(),400);
+            self::messageError("Geral",$e->getMessage(),400);
+        }finally{
+            return [
+                "message" => "Não foi possivel deletar usuário",
+                "status" => "500"
+            ];
         }
     }
 }
-
-UserModel::initConnection();
